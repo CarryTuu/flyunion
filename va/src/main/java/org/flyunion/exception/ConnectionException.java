870fd924c6f44defc5af1633c1476e77e577015b
CarryTuu <12900266+CarryTuu@user.noreply.gitee.com>
2025-04-08 22:46:11 +0800

    完成METAR解析内容

--- va/src/main/java/org/flyunion/exception/ConnectionException.java
+++
@@ -1,9 +0,0 @@
-package org.flyunion.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class ConnectionException extends BaseException{
-	public ConnectionException(String message, HttpStatus status) {
-		super(message, status);
-	}
-}
